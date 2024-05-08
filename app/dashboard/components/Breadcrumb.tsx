768455a2d1bf4ff45a230849d67351507fffe3fc
DJay <diegovt.arbt@gmail.com>
2024-05-07 22:11:58 -0600

    Card and Breadcrumb prototypes

---
+++ app/dashboard/components/Breadcrumb.tsx
@@ -0,0 +1,18 @@
+interface BreadcrumbsProps {
+	breadcrumbs: string[];
+}
+
+export default function Breadcrumb({ breadcrumbs }: BreadcrumbsProps) {
+	return (
+		<div className='border-b-2 border-tertiary/60 py-5 mb-5'>
+			{breadcrumbs.map((breadcrumb, index) => (
+				<span key={breadcrumb} className='text-tertiary'>
+					{breadcrumb}
+					{index !== breadcrumbs.length - 1 && (
+						<span className='mx-2 text-tertiary'>/</span>
+					)}
+				</span>
+			))}
+		</div>
+	);
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,90 @@
 import React from "react";
+import { MoreVertical, UserCheck } from "react-feather";
+import Breadcrumb from "./components/Breadcrumb";
 
 interface Class {
-	institution: string;
-	period: string;
+	company: string;
+	startingDate: string;
+	endDate: string;
 }
 
+function capitalize(string: string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function formatDate(date: string) {
+	const dateObj = new Date(date);
+
+	return capitalize(
+		dateObj
+			.toLocaleDateString("es-ES", { month: "short", year: "numeric" })
+			.replace(".", "")
+	);
+}
+
+const ClassCard: React.FC<Class> = ({ company, startingDate, endDate }) => {
+	return (
+		<div className='w-80'>
+			<div className='text-2xl flex bg-white items-end h-20 rounded-t-lg'>
+				<div className='m-3'>{company}</div>
+			</div>
+			<div className='flex p-3 justify-between items-center bg-primary text-white rounded-b-lg'>
+				<div>
+					{formatDate(startingDate)} - {formatDate(endDate)}
+				</div>
+				<div className='flex'>
+					<UserCheck />
+					<MoreVertical />
+				</div>
+			</div>
+		</div>
+	);
+};
+
 function dashboard() {
-	const scheduledClasses = [];
+	const scheduledClasses = [
+		{
+			company: "Company 1",
+			startingDate: "2021-01-01",
+			endDate: "2021-01-31",
+		},
+		{
+			company: "Company 2",
+			startingDate: "2021-02-01",
+			endDate: "2021-02-28",
+		},
+		{
+			company: "Company 3",
+			startingDate: "2021-03-01",
+			endDate: "2021-03-31",
+		},
+		{
+			company: "Company 4",
+			startingDate: "2021-04-01",
+			endDate: "2021-04-30",
+		},
+		{
+			company: "Company 5",
+			startingDate: "2021-05-01",
+			endDate: "2021-05-31",
+		},
+	];
 
 	return (
 		<div className='flex-1 bg-secondary'>
-			<div> Clases</div>
-			<div></div>
+			<div className='mx-24'>
+				<Breadcrumb breadcrumbs={["Clases"]} />
+				<div className='flex flex-wrap gap-10 justify-start'>
+					{scheduledClasses.map((scheduledClass) => (
+						<ClassCard
+							key={scheduledClass.company}
+							company={scheduledClass.company}
+							startingDate={scheduledClass.startingDate}
+							endDate={scheduledClass.endDate}
+						/>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 }
