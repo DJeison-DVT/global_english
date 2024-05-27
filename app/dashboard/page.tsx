36e9bf07011d5ecaeec56c79ac3d7d9e4dd100f9
DJay <diegovt.arbt@gmail.com>
2024-05-26 21:32:39 -0600

    Admin Dashboard usage of api

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import Header from "../components/Header";
 import { Subjects } from "@/app/utils/consts";
 import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import AdminDashboard from "./components/AdminDashboard";
 
 function ProfessorDashboard() {
 	return (
@@ -19,35 +19,14 @@ function ProfessorDashboard() {
 	);
 }
 
-function AdminDashboard() {
-	return (
-		<Tabs defaultValue='classes' className='w-full h-full flex flex-col'>
-			<TabsList>
-				<TabsTrigger value='classes'>Clases</TabsTrigger>
-				<TabsTrigger value='professors'>Profesores</TabsTrigger>
-			</TabsList>
-			<TabsContent value='classes'>
-				<div className='flex flex-wrap gap-10 justify-start'>
-					{Subjects.map((subject) => (
-						<ClassCard key={subject.id} {...subject} />
-					))}
-				</div>
-			</TabsContent>
-			<TabsContent value='professors'></TabsContent>
-		</Tabs>
-	);
-}
-
 export default async function dashboard() {
 	const session = await getSession();
 	if (!session) redirect("/login");
 
 	return (
-		<div className='flex-1 bg-secondary'>
-			<div className='mx-24'>
-				<div>
-					<Header titles={["Clases"]} links={["/dashboard"]} />
-				</div>
+		<div className='flex-1 flex bg-secondary h-screen w-full'>
+			<div className='mx-24 flex-1 flex-col flex'>
+				<Header titles={["Clases"]} links={["/dashboard"]} />
 				{session.role === "ADMIN" ? <AdminDashboard /> : <ProfessorDashboard />}
 			</div>
 		</div>
