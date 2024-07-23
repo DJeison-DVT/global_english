ee16c44974a0922609ebf8ce050415f868f3112e
Diego Villanueva <diegovt.arbt@gmail.com>
2024-07-23 02:53:30 +0000

    dialog selects current supervisors

--- app/dashboard/components/SupervisorAssignment.tsx
+++ app/dashboard/components/SupervisorAssignment.tsx
@@ -33,6 +33,7 @@ import {
 import { toast } from "@/components/ui/use-toast";
 
 import { getApiURL } from "@/lib/utils";
+import Loading from "@/app/components/Loading";
 
 interface SupervisorAssignmentProps {
 	courses: Course[];
@@ -49,6 +50,8 @@ export default function SupervisorAssignment({
 	const [open, setOpen] = useState(false);
 	const [disabled, setDisabled] = useState(false);
 	const [supervisors, setSupervisors] = useState<User[] | null>(null);
+	const [selectedClassId, setSelectedClassId] = useState<string | null>(null);
+	const [loading, setLoading] = useState(false);
 
 	const form = useForm<z.infer<typeof SupervisorsSchema>>({
 		resolver: zodResolver(SupervisorsSchema),
@@ -68,6 +71,27 @@ export default function SupervisorAssignment({
 		} catch (error) {
 			console.error(error);
 		}
+	};
+
+	const fetchCourseSupervisors = async (courseId: string) => {
+		setLoading(true);
+		try {
+			const response = await fetch(
+				getApiURL(`/api/class/${courseId}/supervisors`)
+			);
+			const data = await response.json();
+			console.log("Assigned supervisors", data);
+			form.setValue(
+				"supervisors",
+				data.map((supervisor: { userId: number }) =>
+					String(supervisor.userId)
+				)
+			);
+			console.log(form.getValues());
+		} catch (error) {
+			console.error(error);
+		}
+		setLoading(false);
 	};
 
 	const onSubmit = async (data: any) => {
@@ -90,14 +114,14 @@ export default function SupervisorAssignment({
 
 			toast({
 				title: "Success",
-				description: "Supervisors assigned successfully",
+				description: "Supervisores asignados correctamente",
 			});
 			setOpen(false);
 		} catch (error) {
 			console.error(error);
 			toast({
 				title: "Error",
-				description: "Failed to assign supervisors",
+				description: "Error al asignar supervisores",
 			});
 		} finally {
 			setDisabled(false);
@@ -108,7 +132,11 @@ export default function SupervisorAssignment({
 		if (!supervisors) {
 			fetchSupervisors();
 		}
-	}, [courses]);
+
+		if (selectedClassId) {
+			fetchCourseSupervisors(selectedClassId);
+		}
+	}, [selectedClassId, courses]);
 
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
@@ -132,7 +160,10 @@ export default function SupervisorAssignment({
 									<FormLabel>Clase</FormLabel>
 									<FormControl>
 										<Select
-											onValueChange={field.onChange}
+											onValueChange={(value) => {
+												field.onChange(value);
+												setSelectedClassId(value);
+											}}
 											value={field.value}
 										>
 											<SelectTrigger className="w-[180px]">
@@ -161,78 +192,90 @@ export default function SupervisorAssignment({
 								</FormItem>
 							)}
 						/>
-						<FormField
-							control={form.control}
-							name="supervisors"
-							render={() => (
-								<FormItem className="w-full flex-1">
-									<FormLabel className="text-base">
-										Supervisores Disponibles
-									</FormLabel>
-									{supervisors &&
-										supervisors.map((supervisor) => (
-											<FormField
-												key={supervisor.id}
-												control={form.control}
-												name="supervisors"
-												render={({ field }) => {
-													return (
-														<FormItem
+						<div className="w-full flex-1">
+							<Loading active={loading}>
+								<FormField
+									control={form.control}
+									name="supervisors"
+									render={() => (
+										<FormItem className="h-full w-full">
+											<FormLabel className="text-base">
+												Supervisores Disponibles
+											</FormLabel>
+											{supervisors &&
+												supervisors.map(
+													(supervisor) => (
+														<FormField
 															key={supervisor.id}
-															className="flex flex-row items-start space-x-3 space-y-0"
-														>
-															<FormControl>
-																<Checkbox
-																	checked={field.value?.includes(
-																		String(
+															control={
+																form.control
+															}
+															name="supervisors"
+															render={({
+																field,
+															}) => {
+																return (
+																	<FormItem
+																		key={
 																			supervisor.id
-																		)
-																	)}
-																	onCheckedChange={(
-																		checked
-																	) => {
-																		return checked
-																			? field.onChange(
-																					[
-																						...field.value,
-																						String(
-																							supervisor.id
-																						),
-																					]
-																			  )
-																			: field.onChange(
-																					field.value?.filter(
-																						(
-																							value
-																						) =>
-																							value !==
-																							String(
-																								supervisor.id
-																							)
+																		}
+																		className="flex flex-row items-start space-x-3 space-y-0"
+																	>
+																		<FormControl>
+																			<Checkbox
+																				checked={field.value?.includes(
+																					String(
+																						supervisor.id
 																					)
-																			  );
-																	}}
-																/>
-															</FormControl>
-															<FormLabel className="font-normal">
-																{
-																	supervisor.name
-																}
-															</FormLabel>
-														</FormItem>
-													);
-												}}
-											/>
-										))}
-									<FormMessage>
-										{
-											form.formState.errors.supervisors
-												?.message
-										}
-									</FormMessage>
-								</FormItem>
-							)}
-						/>
+																				)}
+																				onCheckedChange={(
+																					checked
+																				) => {
+																					return checked
+																						? field.onChange(
+																								[
+																									...field.value,
+																									String(
+																										supervisor.id
+																									),
+																								]
+																						  )
+																						: field.onChange(
+																								field.value?.filter(
+																									(
+																										value
+																									) =>
+																										value !==
+																										String(
+																											supervisor.id
+																										)
+																								)
+																						  );
+																				}}
+																			/>
+																		</FormControl>
+																		<FormLabel className="font-normal">
+																			{
+																				supervisor.name
+																			}
+																		</FormLabel>
+																	</FormItem>
+																);
+															}}
+														/>
+													)
+												)}
+											<FormMessage>
+												{
+													form.formState.errors
+														.supervisors?.message
+												}
+											</FormMessage>
+										</FormItem>
+									)}
+								/>
+							</Loading>
+						</div>
 						<Button type="submit" disabled={disabled}>
 							Asignar Supervisores
 						</Button>
